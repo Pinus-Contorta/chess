0b584be588f83fc0ee54dd3e1784a6552d30653a
Pinus-Contorta <nalongo@byu.edu>
2026-06-29 14:40:06 -0600

    impl(p0): All tests passed except for PAWN.

--- shared/src/main/java/chess/ChessMoveCalculator.java
+++ shared/src/main/java/chess/ChessMoveCalculator.java
@@ -11,8 +11,8 @@ public class ChessMoveCalculator {
 
     private static final int[][] KING_DIR = {{1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}};
     private static final int[][] QUEEN_DIR = {{1,0},{1,1},{0,1},{-1,1},{-1,0},{-1,-1},{0,-1},{1,-1}};
-    private static final int[][] BISHIOP_DIR = {{-1,-1},{-1,1},{1,-1},{1,1}};
-    private static final int[][] KNIGHT_DIR = {{1,2},{2,1},{-1,2},{-2,1},{-1,-2},{-2,-1},{-1,2},{-2,1}};
+    private static final int[][] BISHOP_DIR = {{-1,-1},{-1,1},{1,-1},{1,1}};
+    private static final int[][] KNIGHT_DIR = {{1,2},{2,1},{-1,2},{-2,1},{-1,-2},{-2,-1},{1,-2},{2,-1}};
     private static final int[][] ROOK_DIR = {{1,0},{0,1},{-1,0},{0,-1}};
 
 
@@ -25,7 +25,7 @@ public class ChessMoveCalculator {
 
 
     // This method takes the start position and the piece being moved and generates a list of all legal moves, barring those which would put the king piece in danger
-    //TODO: In the middle of implimenting the BISHOP rule cases. Finish this and the rest.
+    //TODO: In the middle of implimenting the PAWN. Needs to handle promotion.
     public Collection<ChessMove> getValidMoves(ChessBoard board,ChessPosition startPosition, ChessPiece piece) {
 
         List<ChessMove> validMoves = new ArrayList<>();
@@ -64,8 +64,7 @@ public class ChessMoveCalculator {
                 return validMoves;
 
             case BISHOP:
-                //return List.of(new ChessMove(startPosition, new ChessPosition(2,2), null));
-                for (int[] dir : BISHIOP_DIR){
+                for (int[] dir : BISHOP_DIR){
                     int row = startPosition.getRow() + dir[0];
                     int col = startPosition.getColumn() + dir[1];
 
@@ -83,10 +82,35 @@ public class ChessMoveCalculator {
                 return validMoves;
 
             case KNIGHT:
-                return List.of(new ChessMove(startPosition, new ChessPosition(3,3), null));
+                for(int[] dir : KNIGHT_DIR){
+                    int row = startPosition.getRow() + dir[0];
+                    int col = startPosition.getColumn() + dir[1];
+
+                    ChessPosition target = new ChessPosition(row, col);
+                    if (board.inBounds(row,col)) {
+                        isValidMove(board, startPosition, piece, validMoves, target);
+                    }
+
+                }
+                return validMoves;
 
             case ROOK:
-                return List.of(new ChessMove(startPosition, new ChessPosition(4,4), null));
+                for (int[] dir : ROOK_DIR){
+                    int row = startPosition.getRow() + dir[0];
+                    int col = startPosition.getColumn() + dir[1];
+
+                    while (board.inBounds(row, col)) {
+                        ChessPosition target = new ChessPosition(row, col);
+
+                        if (isValidMove(board, startPosition, piece, validMoves, target)) {
+                            break;
+                        }
+
+                        row += dir[0];
+                        col += dir[1];
+                    }
+                }
+                return validMoves;
 
             case PAWN:
                 return List.of(new ChessMove(startPosition, new ChessPosition(5,5), null));
