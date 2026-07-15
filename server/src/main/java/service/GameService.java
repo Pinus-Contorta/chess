ec65d64709c5e27c483ad9faa9303dd6d916ff7e
Count-Iblis <134972494+Count-Iblis@users.noreply.github.com>
2026-07-15 15:27:35 -0600

    impl(p3): GameService implimented to handle game listing, game instantiation, and joining games. Need to write tests for game listing next.

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,4 +1,35 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameService {
+
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public GameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public ListGamesResult listGames(String authToken) throws DataAccessException {
+        if (authToken == null || authDAO.getAuth(authToken) == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        List<GameTag> summaries = new ArrayList<>();
+        for (GameData game : gameDAO.listGames()) {
+            summaries.add(new GameTag(
+                    game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName()));
+        }
+
+        return new ListGamesResult(summaries);
+
+    }
 }
---
+++ server/src/main/java/service/GameTag.java
@@ -0,0 +1,4 @@
+package service;
+
+public record GameTag(int gameID, String whiteUser, String blackUser, String gameName) {
+}
