9b7ba8ca26d29af3ad6c06777ed28f8c591a0f9a
Naradus <89316889+Naradus@users.noreply.github.com>
2026-07-28 21:04:01 -0600

    impl(p5): PreLoginClient and PostLoginClient fleshed out.
    Signed-off-by: Naradus <89316889+Naradus@users.noreply.github.com>

--- client/src/main/java/client/PostLoginClient.java
+++ client/src/main/java/client/PostLoginClient.java
@@ -1,10 +1,65 @@
 package client;
 
 public class PostLoginClient {
+
+    private final ServerFacade serverFacade;
+    private final ChessClient chessClient;
+
     public PostLoginClient(ServerFacade serverFacade, ChessClient chessClient) {
+        this.serverFacade = serverFacade;
+        this.chessClient = chessClient;
     }
 
-    public String eval(String inputString){
-        return "";
+    public String eval(String inputString) {
+
+        var tokens = inputString.trim().split("\\s+");
+        var command = tokens.length > 0 ? tokens[0].toLowerCase() : "help";
+
+        return switch (command) {
+            case "create" -> createGame(tokens);
+            case "list" -> listGames();
+            case "join" -> joinGame(tokens);
+            case "observe" -> observeGame(tokens);
+            case "logout" -> logout();
+            case "quit" -> quit();
+            default -> help();
+        };
     }
+
+    private String help() {
+        return """
+                create <NAME> - a game
+                list - games
+                join <ID> [WHITE|BLACK] - a game
+                observe <ID> - a game
+                logout - when you are done
+                quit - playing chess
+                help - with possible commands
+                """;
+    }
+
+    private String createGame(String[] tokens) {
+        return "not yet implemented";
+    }
+
+    private String listGames() {
+        return "not yet implemented";
+    }
+
+    private String joinGame(String[] tokens) {
+        return "not yet implemented";
+    }
+
+    private String observeGame(String[] tokens) {
+        return "not yet implemented";
+    }
+
+    private String logout() {
+        return "not yet implemented";
+    }
+
+    private String quit() {
+        return "not yet implemented";
+    }
+
 }
--- client/src/main/java/client/PreLoginClient.java
+++ client/src/main/java/client/PreLoginClient.java
@@ -1,10 +1,45 @@
 package client;
 
+import java.util.Locale;
+
 public class PreLoginClient {
+
+    private final ServerFacade serverFacade;
+    private final ChessClient chessClient;
     public PreLoginClient(ServerFacade serverFacade, ChessClient chessClient) {
+        this.serverFacade = serverFacade;
+        this.chessClient = chessClient;
     }
 
     public String eval(String inputString){
-        return "";
+
+        var tokens = inputString.trim().split("\\s+");
+        var command = tokens.length > 0 ? tokens[0].toLowerCase() : "help";
+
+        return switch (command) {
+            case "quit" -> "quit";
+            case "login" -> login(tokens);
+            case "register" -> register(tokens);
+            default -> help();
+        };
+    }
+
+    private String help() {
+        return """
+                register <USERNAME> <PASSWORD> <EMAIL> - to create a new account
+                login <USERNAME> <PASSWORD> - to play chess
+                quit - playing chess
+                help - with possible commands
+                """;
+    }
+
+    private String login(String[] tokens) {
+        //TODO:Implement PreLoginClient.login
+        return"";
+    }
+
+    private String register(String[] tokens) {
+        //TODO:Implement PreLoginClient.register
+        return"";
     }
 }
