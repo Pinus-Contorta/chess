5f4fd9973c1fb5c49dfaf84166af1c3a93bfa22e
Naradus <89316889+Naradus@users.noreply.github.com>
2026-08-05 21:57:49 -0600

    impl(p6): Implemented ConnectionManager.java in my server directory.
    Signed-off-by: Naradus <89316889+Naradus@users.noreply.github.com>

---
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -0,0 +1,53 @@
+package websocket;
+
+import com.google.gson.Gson;
+import io.javalin.websocket.WsContext;
+import websocket.messages.ServerMessage;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ConnectionManager {
+
+    private record Connection(String username, WsContext session) {}
+
+    private final Map<Integer, List<Connection>> gameConnections = new ConcurrentHashMap<>();
+    private final Gson gson = new Gson();
+
+    public void add(int gameID, String username, WsContext session) {
+        gameConnections.computeIfAbsent(gameID, id -> new CopyOnWriteArrayList<>())
+                .add(new Connection(username, session));
+    }
+
+    public void remove(WsContext session) {
+        for(List<Connection> connections : gameConnections.values()) {
+            connections.removeIf(connection -> connection.session().equals(session));
+        }
+    }
+
+    public void sendToRobot(WsContext session, ServerMessage message) {
+        session.send(gson.toJson(message));
+    }
+
+    public void broadcast(int gameID, String excludeUsername, ServerMessage message) {
+        var connections = gameConnections.get(gameID);
+
+        if(connections == null) {
+            return;
+        }
+
+        String json = gson.toJson(message);
+
+        for (Connection connection : connections) {
+            if(connection.username().equals(excludeUsername)){
+                continue;
+            }
+
+            try{
+                connection.session().send(json);
+            } catch (Exception ignored) {}
+        }
+    }
+}
